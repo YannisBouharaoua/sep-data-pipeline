67dc9c7606ebd7059ec53eeae52352eb0c4248ef
YannisBouharaoua <169106007+mortycold@users.noreply.github.com>
2026-03-11 03:33:26 +0100

    fix: add L04AX07 code for dimethyl fumarate reclassification post-2019

--- sep_dbt/models/staging/stg_openmedic_sep.sql
+++ sep_dbt/models/staging/stg_openmedic_sep.sql
@@ -1,5 +1,5 @@
 {% set sep_codes = ['L03AB04','L03AB05','L03AB07','L03AB08','L03AB11','L03AB13',
-                    'L04AA27','L04AA31','N07XX09'] %}
+                    'L04AA27','L04AA31','L04AX07','N07XX09'] %}
 
 {% for year in [2019, 2020, 2021, 2022] %}
 SELECT {{ year }} AS annee, ATC5, L_ATC5, ATC3, L_ATC3,
